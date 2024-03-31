e306e3fa442cc9f1c70abb5e755bc2f8f5864f76
e-lumos <e_lumos@kakao.com>
2024-03-31 15:39:26 +0900

    PS: 2475/CPP

---
+++ 2475/CPP/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    int A, B, C, D, E;
+    cin >> A >> B >> C >> D >> E;
+
+    cout << (A * A + B * B + C * C + D * D + E * E) % 10;
+
+    return 0;
+}
