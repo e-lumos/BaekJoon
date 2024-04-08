b4c2abedab748203d2de823488649eff14358141
e-lumos <e_lumos@kakao.com>
2024-04-08 20:21:10 +0900

    PS: 10998/CPP

---
+++ 10998/CPP/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    int A, B;
+    cin >> A >> B;
+    cout << A * B;
+
+    return 0;
+}
