3c1817d2b93c9ef8f28a92d446de893a8780a98a
e-lumos <e_lumos@kakao.com>
2024-04-14 02:51:57 +0900

    PS: 10172/CPP

---
+++ 10172/CPP/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cout << "|\\_/|\n"
+            "|q p|   /}\n"
+            "( 0 )\"\"\"\\\n"
+            "|\"^\"`    |\n"
+            "||_/=\\\\__|";
+    return 0;
+}
